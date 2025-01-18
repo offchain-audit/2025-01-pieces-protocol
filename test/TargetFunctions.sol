59e2aa1bc7e6d53b142b16d8be544492be9e9707
s0crates <142903331+offchain-audit@users.noreply.github.com>
2025-01-18 10:21:08 -0500

    fuzz and invariant testing handlers

---
+++ test/TargetFunctions.sol
@@ -0,0 +1,11 @@
+
+// SPDX-License-Identifier: GPL-2.0
+pragma solidity ^0.8.0;
+
+import {BaseTargetFunctions} from "@chimera/BaseTargetFunctions.sol";
+import {Properties} from "./Properties.sol";
+import {vm} from "@chimera/Hevm.sol";
+
+abstract contract TargetFunctions is BaseTargetFunctions, Properties {
+
+}
